f2c1e3a9a3a5035ee356fd2045888771c4746f7a
Blackskull2007 <Malachinmakayla@gmail.com>
2024-09-20 14:50:18 -0400

    9/20/24 code

---
+++ 01_gaming_exercises/04_rock_paper_scissors/RockPaperScissors.cs
@@ -0,0 +1,15 @@
+using System;
+
+class RockPaperScissors {
+  static void Main(){
+
+
+int games = 10
+string cpuChoice
+string playerChoice
+console.WriteLine("Enter PLAYER NAME"\n);
+Random rnd = new Random();
+if (cpuChoice = rock)
+
+  }
+}
